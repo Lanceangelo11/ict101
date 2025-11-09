834a5558617d5c09944d1a7aa70d86c31feb99f8
Lanceangelo11 <lanceangelo16w@gmail.com>
2025-11-09 20:52:50 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+ public class Main{
+     public static void main(String[] args) {
+        JFrame access = new JFrame();
+        frame.setTitle("Crossfire cheat");
+        frame.setVisible(true);
+     }
+
+
+ }
+
